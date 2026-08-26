cf4e456b779b9fec67579cd0a723c486c7f30ebe
Han821 <hanahmad821@gmail.com>
2026-08-26 09:14:58 +0700

    Create academic year master

---
+++ app/Models/AcademicYear.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class AcademicYear extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'start_date',
+        'end_date',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'start_date' => 'date',
+            'end_date' => 'date',
+            'is_active' => 'boolean',
+        ];
+    }
+}
