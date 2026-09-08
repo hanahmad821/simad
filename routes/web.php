3f8ad41153f2d4dedd1a87c8cfd52e73c8d67e52
Han821 <hanahmad821@gmail.com>
2026-09-08 11:50:58 +0700

    refactor route

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,60 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-});
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/admin', function () {
-        return 'Admin';
-    });
-});
-Route::middleware(['auth', 'role:teacher'])->group(function () {
-    Route::get('/teacher', function () {
-        return 'Teacher';
-    });
-});
-Route::middleware(['auth', 'role:headmaster'])->group(function () {
-    Route::get('/headmaster', function () {
-        return 'Headmaster';
-    });
-});
-Route::middleware(['auth', 'role:admin,headmaster'])->group(function () {
-    Route::get('/reports', function () {
-        return 'Reports';
-    });
+
+    // Redirect dashboard berdasarkan role
+    Route::get('/dashboard', function () {
+        return match (auth()->user()->role) {
+            'admin' => redirect()->route('admin.dashboard'),
+            'headmaster' => redirect()->route('headmaster.dashboard'),
+            'teacher' => redirect()->route('teacher.dashboard'),
+            'staff' => redirect()->route('staff.dashboard'),
+        };
+    })->name('dashboard');
+
+
+    // Admin
+    Route::middleware('role:admin')
+        ->prefix('admin')
+        ->name('admin.')
+        ->group(function () {
+
+            Route::view('/dashboard', 'admin.dashboard')
+                ->name('dashboard');
+        });
+
+
+    // Kepala Madrasah
+    Route::middleware('role:headmaster')
+        ->prefix('headmaster')
+        ->name('headmaster.')
+        ->group(function () {
+
+            Route::view('/dashboard', 'headmaster.dashboard')
+                ->name('dashboard');
+        });
+
+
+    // Guru
+    Route::middleware('role:teacher')
+        ->prefix('teacher')
+        ->name('teacher.')
+        ->group(function () {
+
+            Route::view('/dashboard', 'teacher.dashboard')
+                ->name('dashboard');
+        });
+
+
+    // Staff
+    Route::middleware('role:staff')
+        ->prefix('staff')
+        ->name('staff.')
+        ->group(function () {
+
+            Route::view('/dashboard', 'staff.dashboard')
+                ->name('dashboard');
+        });
 });
 
 require __DIR__.'/settings.php';
