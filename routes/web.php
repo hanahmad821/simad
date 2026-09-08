41b4a0ac3edd87c412e923b29609b10b4a3e9e98
Han821 <hanahmad821@gmail.com>
2026-09-08 11:45:40 +0700

    add role middleware

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,25 @@ Route::view('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin', function () {
+        return 'Admin';
+    });
+});
+Route::middleware(['auth', 'role:teacher'])->group(function () {
+    Route::get('/teacher', function () {
+        return 'Teacher';
+    });
+});
+Route::middleware(['auth', 'role:headmaster'])->group(function () {
+    Route::get('/headmaster', function () {
+        return 'Headmaster';
+    });
+});
+Route::middleware(['auth', 'role:admin,headmaster'])->group(function () {
+    Route::get('/reports', function () {
+        return 'Reports';
+    });
+});
 
 require __DIR__.'/settings.php';
